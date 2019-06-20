c7c424a0a75320dc8b9002ea58d332a648aa2134
gillianwenhold <gwenhold@mac.com>
2019-06-19 22:20:44 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,49 +17,49 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+  return holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+   holiday_hash[:winter].each do |holiday, list|
+    list.push(supply)
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  return holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  holiday_hash.each do |season, data|
+    puts season.to_s.capitalize! + ":"
+    data.each do |holiday, items|
+      capitalized = holiday.to_s.split("_").collect{|word| word.capitalize!}
+      puts "  #{capitalized.join(" ")}: #{items.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  list = []
+  holiday_hash.each do |season, data|
+    data.each do |day, items|
+      if items.include?("BBQ")
+        list << day
+      end
+    end
+  end
+  list
 end
 
 
